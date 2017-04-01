5a0112f8bc25a40ab3ed068f3c0cd4280ec04d53
acr <acarbey15@gmail.com>
2017-04-01 23:34:43 +0300

    Yaz

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,16 +1,7 @@
 <?php
 use Illuminate\Http\Request;
 
-Route::get('/destek/{sayfa}/', function (Request $request, $sayfa) {
-    $tab      = $request->input('tab');
-    $mesaj_id = $request->input('mesaj_id');
-    $sayfa    = empty($sayfa) ? 'anasayfa' : $sayfa;
-    $msg      = $request->input('msg');
-
-    return Destek::index($sayfa, null, $tab, $mesaj_id, $msg);
-})->middleware('auth');
-
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['auth']], function () {
     Route::get('/destek/{sayfa}/{yer}', function (Request $request, $sayfa, $yer) {
         $tab      = $request->input('tab');
         $sayfa    = empty($sayfa) ? 'anasayfa' : $sayfa;
@@ -19,7 +10,14 @@ Route::group(['middleware' => ['web']], function () {
         $msg      = $request->input('msg');
         return Destek::index($sayfa, $yer, $tab, $mesaj_id, $msg);
     });
+    Route::get('/destek/{sayfa}/', function (Request $request, $sayfa) {
+        $tab      = $request->input('tab');
+        $mesaj_id = $request->input('mesaj_id');
+        $sayfa    = empty($sayfa) ? 'anasayfa' : $sayfa;
+        $msg      = $request->input('msg');
 
+        return Destek::index($sayfa, null, $tab, $mesaj_id, $msg);
+    })->middleware('auth');
     Route::get('/destek', function (Request $request) {
         $tab      = $request->input('tab');
         $sayfa    = 'anasayfa';
